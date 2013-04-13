f104370bf2b0960f5d7d67f0a9767cb7c776ec8f
andyba45 <andyba45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-13 05:14:42 +0000

    Added admin Dashboard widget
    git-svn-id: https://plugins.svn.wordpress.org/double-opt-in-for-download/trunk@696878 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/doifd-admin.php
+++ admin/doifd-admin.php
@@ -628,4 +628,43 @@ function doifd_download_page() {
                 }
             }
         }
+        
+function doifd_lab_dashboard_widget_function() {
+    
+    global $wpdb;
+    
+    // get the total count of subscribers
+            $sql = "SELECT COUNT(*) FROM " . $wpdb->prefix . "doifd_lab_subscribers " ;
+            $doifd_subscriber_count = $wpdb->get_var ( $sql ) ;
+            
+   // get the total count of downloads
+            $sql = "SELECT SUM(doifd_number_of_downloads) FROM " . $wpdb->prefix . "doifd_lab_downloads " ;
+            $doifd_download_count = $wpdb->get_var ( $sql ) ;
+            
+   // get downloads
+            $sql = "SELECT * FROM " . $wpdb->prefix . "doifd_lab_downloads " ;
+            $doifd_downloads_result = $wpdb->get_results ( $sql , ARRAY_A ) ;
+            
+	// Display whatever it is you want to show
+        echo '<table class="doifd_admin_widget_table">';
+        echo '<tr>';
+        echo '<th class="doifd_admin_widget_th">Total Subscribers: ' . $doifd_subscriber_count . '</th>';
+	echo '<th class="doifd_admin_widget_th">Overall Total Downloads: ' . $doifd_download_count . '</th>';
+        echo '</tr>';
+        foreach ($doifd_downloads_result as $value) {
+            echo '<tr>';
+            echo "<td class='doifd_admin_widget_td'>" . $value[ 'doifd_download_name' ] . "</td>";
+            echo "<td class='doifd_admin_widget_td'>" . $value[ 'doifd_number_of_downloads' ] . "</td>";
+            echo '</tr>';
+        }
+        echo '</table>';
+}
+        
+function doifd_lab_add_dashboard_widgets() {
+    wp_add_dashboard_widget('doifd_dashboard_widget', 'Double Opt-In For Downloads', 'doifd_lab_dashboard_widget_function');	
+} 
+
+// Hook into the 'wp_dashboard_setup' action to register our other functions
+
+add_action('wp_dashboard_setup', 'doifd_lab_add_dashboard_widgets' );
         ?>
