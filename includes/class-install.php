5a560f92e735aab1720bb62af0f35cd735f95839
andyba45 <andyba45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-19 23:25:29 +0000

    Updated version number in install file
    git-svn-id: https://plugins.svn.wordpress.org/double-opt-in-for-download/branches/working@790648 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-install.php
+++ includes/class-install.php
@@ -32,7 +32,7 @@ if ( !class_exists ( 'DoifdInstall' ) ) {
 
             /* If version number is different create/update plugin tables */
 
-            $current_version = '1.0.6';
+            $current_version = '1.0.8';
 
             if ( $doifd_lab_installed_ver != $current_version ) {
 
