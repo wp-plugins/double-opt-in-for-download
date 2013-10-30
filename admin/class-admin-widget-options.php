83314b57e3edca021ebcacc14d1147cb22be3277
andyba45 <andyba45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-30 02:03:05 +0000

    Version 1.0.8
    git-svn-id: https://plugins.svn.wordpress.org/double-opt-in-for-download/tags/1.0.8@795627 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/class-admin-widget-options.php
@@ -0,0 +1,300 @@
+<?php
+
+if ( !class_exists ( 'DoifdAdminWidgetOptions' ) ) {
+
+    class DoifdAdminWidgetOptions {
+
+        public function __construct() {
+            
+        }
+
+        public static function field_widget_width() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_width' ] ) && ($doifd_option[ 'widget_width' ] == !NULL ) ) {
+
+                $widget_width = $doifd_option[ 'widget_width' ];
+            } else {
+
+                $widget_width = '190';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_width]" id="widget_width"  size="4" value="' . $widget_width . '">';
+            echo '<p>' . __( 'This is the width of the widget in the sidebar. <b>Use numbers only, DO NOT ADD the px at the end.' , 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_widget_inside_padding() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_inside_padding' ] ) && ($doifd_option[ 'widget_inside_padding' ] == !NULL ) ) {
+
+                $widget_inside_padding = $doifd_option[ 'widget_inside_padding' ];
+            } else {
+
+                $widget_inside_padding = '5';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_inside_padding]" id="widget_inside_padding" size="4" value="' . $widget_inside_padding . '">';
+            echo '<p>' . __( 'This is the amount of padding used inside of the widget form. <b>Use numbers only, DO NOT ADD the px at the end.' , 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_widget_top_margin() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_margin_top' ] ) && ($doifd_option[ 'widget_margin_top' ] == !NULL ) ) {
+
+                $widget_margin_top = $doifd_option[ 'widget_margin_top' ];
+            } else {
+
+                $widget_margin_top = '25';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_margin_top]" id="widget_margin_top" size="4" value="' . $widget_margin_top . '">';
+            echo '<p>' . __( 'This is the top margin of the widget. <b>Use numbers only, DO NOT add the px at the end.',  'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_widget_right_margin() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_margin_right' ] ) && ($doifd_option[ 'widget_margin_right' ] == !NULL ) ) {
+
+                $widget_margin_right = $doifd_option[ 'widget_margin_right' ];
+            } else {
+
+                $widget_margin_right = '0';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_margin_right]" id="widget_margin_right" size="4" value="' . $widget_margin_right . '">';
+            echo '<p>' . __( 'This is the right margin of the widget. <b>Use numbers only, DO NOT add the px at the end.', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_widget_bottom_margin() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_margin_bottom' ] ) && ($doifd_option[ 'widget_margin_bottom' ] == !NULL ) ) {
+
+                $widget_margin_bottom = $doifd_option[ 'widget_margin_bottom' ];
+            } else {
+
+                $widget_margin_bottom = '25';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_margin_bottom]" id="widget_margin_bottom" size="4" value="' . $widget_margin_bottom . '">';
+            echo '<p>' . __( 'This is the bottom margin of the widget. <b>Use numbers only, DO NOT add the px at the end.', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_widget_left_margin() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_margin_left' ] ) && ($doifd_option[ 'widget_margin_left' ] == !NULL ) ) {
+
+                $widget_margin_left = $doifd_option[ 'widget_margin_left' ];
+            } else {
+
+                $widget_margin_left = '0';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_margin_left]" id="widget_margine_left"  size="4" value="' . $widget_margin_left . '">';
+            echo '<p>' . __( 'This is the left margin of the widget. <b>Use numbers only, DO NOT add the px at the end.', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+
+        public static function field_input_field_width() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_input_width' ] ) && ($doifd_option[ 'widget_input_width' ] == !NULL ) ) {
+
+                $widget_input_width = $doifd_option[ 'widget_input_width' ];
+            } else {
+
+                $widget_input_width = '180';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_input_width]" id="widget_input_width" size="4" value="' . $widget_input_width . '">';
+            echo '<p>' . __( 'This sets the width of the input field on the widget. <b>Use numbers only, DO NOT add the px at the end.', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+        
+        public static function field_widget_background_color() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get widget stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_background_color' ] ) && ($doifd_option[ 'widget_background_color' ] == !NULL ) ) {
+
+                $widget_background_color = $doifd_option[ 'widget_background_color' ];
+            } else {
+
+                $widget_background_color = 'transparent';
+            }
+
+            /* echo widget width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_background_color]" id="widget_background_color" size="10" value="' . $widget_background_color . '">';
+            echo '<p>' . __( 'This sets the background color of the text input fields. <b>You can use transparent or hex values ( #000000 etc ).', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+        
+        public static function field_widget_input_field_background_color() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option ( 'doifd_lab_options' );
+
+            /* get form stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset ( $doifd_option[ 'widget_input_field_background_color' ] ) && ($doifd_option[ 'widget_input_field_background_color' ] == !NULL ) ) {
+
+                $widget_input_field_background_color = $doifd_option[ 'widget_input_field_background_color' ];
+            } else {
+
+                $widget_input_field_background_color = 'transparent';
+            }
+
+            /* echo form width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_input_field_background_color]" id="widget_input_field_background_color" size="10" value="' . $widget_input_field_background_color . '">';
+            echo '<p>' . __( 'This sets the background color of the text input fields. <b>You can use transparent or hex values ( #000000 etc ).', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+
+        }
+        
+        public static function field_widget_title_color() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option( 'doifd_lab_options' );
+
+            /* get form stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset( $doifd_option[ 'widget_title_color' ] ) && ($doifd_option[ 'widget_title_color' ] == !NULL ) ) {
+
+                $widget_title_color = $doifd_option[ 'widget_title_color' ];
+            } else {
+
+                $widget_title_color = '#000000';
+            }
+
+            /* echo form width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_title_color]" id="widget_title_color"  size="10" value="' . $widget_title_color . '">';
+            echo '<p>' . __( 'This sets the font color for the widget title. <b>Use hex values ( #000000 etc ).', 'double-opt-in-for-download' ) . '</b></p>';
+            echo '</div>';
+        }
+        
+        public static function field_widget_title_size() {
+
+            /* get the options from wp options table */
+
+            $doifd_option = get_option( 'doifd_lab_options' );
+
+            /* get form stored field value and assign to variable.
+             * If set or not null use the stored option otherwise use the default */
+
+            if ( isset( $doifd_option[ 'widget_title_size' ] ) && ($doifd_option[ 'widget_title_size' ] == !NULL ) ) {
+
+                $widget_title_size = $doifd_option[ 'widget_title_size' ];
+            } else {
+
+                $widget_title_size = '1em';
+            }
+
+            /* echo form width form */
+
+            echo '<div id="doifd_lab_admin_options">';
+            echo '<input type="text" name="doifd_lab_options[widget_title_size]" id="widget_title_size"  size="10" value="' . $widget_title_size . '">';
+            echo '<p>' . __( 'This sets the font size for the widget title. ( Example: 1em, 12px etc ).', 'double-opt-in-for-download' ) . '</p>';
+            echo '</div>';
+        }
+
+
+    }
+
+}
+
+?>
---
+++ js/admin.js
@@ -0,0 +1,81 @@
+/* Delete Confirmation for subscribers and downloads */
+
+jQuery(document).ready(function() {
+    jQuery("#dialog").dialog({
+        autoOpen: false,
+        modal: true
+    });
+
+    jQuery(".confirm").click(function(e) {
+        e.preventDefault();
+        var targetUrl = jQuery(this).attr("href");
+
+        jQuery("#dialog").dialog({
+            buttons: {
+                "Delete": function() {
+                    window.location.href = targetUrl;
+                },
+                "Cancel": function() {
+                    jQuery(this).dialog("close");
+                }
+            }
+        });
+
+        jQuery("#dialog").dialog("open");
+    });
+});
+
+/* Facebook Script */
+
+jQuery(document).ready(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id))
+        return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=316169008473308";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/* Admin Options Tabs Script */
+
+jQuery(function() {
+    jQuery("#tabs").tabs({
+        beforeLoad: function(event, ui) {
+            ui.jqXHR.error(function() {
+                ui.panel.html(
+                        "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+                        "If this wouldn't be a demo.");
+            });
+        }
+    });
+});
+
+/* Hide Privacy Options if not seclected */
+
+jQuery(document).ready(function($){
+    
+     $("#priv_text").hide();
+     $("#priv_font_size").hide();
+     $("#priv_sel_pag").hide();
+    if ($("#use_privacy_policy_y:checked").length > 0){
+        $("#priv_text").show();
+        $("#priv_font_size").show();
+        $("#priv_sel_pag").show();
+    
+    }
+
+	$('#use_privacy_policy_y').click(function(){
+		$('#priv_text').show();
+                $("#priv_font_size").show();
+                $("#priv_sel_pag").show();
+	});
+
+	$('#use_privacy_policy_n').click(function(){
+		$('#priv_text').hide();
+                $("#priv_font_size").hide();
+                $("#priv_sel_pag").hide();
+	});
+
+});
+
