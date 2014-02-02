0528ee69e01f4d4a34ce85ddedb3ace0c36c5023
andyba45 <andyba45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-02 18:06:21 +0000

    Version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/double-opt-in-for-download/tags/1.1.1@850058 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/view-admin-header.php
@@ -0,0 +1,17 @@
+<div id="doifd_admin_header">
+    <div id="fb-root"></div>
+    <h1><a href="http://www.doubleoptinfordownload.com/" target="new" ><?php _e( 'Double OPT-IN for Downloads' , 'double-opt-in-for-download' ); ?></a> - <a href="http://www.labwebdesigns.com/" target="new" >LAB Web Development</a></h1>
+    <br />
+    <h2><?php _e( 'It takes a village to build a plugin!', 'double-opt-in-for-download' ); ?></h2>
+    <p><?php _e( 'If you need assistance with this plugin, or you have some thoughts and suggestions<br />on how to improve this plugin please visit the' , 'double-opt-in-for-download' ); ?> <a href="http://www.doubleoptinfordownload.com/forums/forum/double-opt-in-for-download-support/" target="new"><?php _e( 'support page' , 'double-opt-in-for-download' ); ?></a></p>
+    <p><?php _e( 'If you like this plugin please review it', 'double-opt-in-for-download' ); ?> <a href="http://wordpress.org/support/view/plugin-reviews/double-opt-in-for-download" target="new"><?php _e( 'here', 'double-opt-in-for-download' ); ?></a></p>
+    <h3><?php _e( 'Your are using the <em>FREE</em> Version 1.1.1', 'double-opt-in-for-download' ); ?></h3> 
+    <h3><a href="http://www.doubleoptinfordownload.com/premium-double-opt-in-for-download/" target="new" ><?php _e( 'Get the Premium Version of Double OPT-IN for download', 'double-opt-in-for-download' ); ?></a></h3>
+    
+    <div class="fb-like" data-href="https://www.facebook.com/pages/Double-Opt-In-For-Download/371589009609336" data-send="true" data-width="450" data-show-faces="true"></div>
+    
+    <div class="doifd_admin_paypal">
+        <h1><?php _e( '"If you like the free version of Double OPT-IN For Download, Then you will LOVE the Premium Version"<br>Check it out,', 'double-opt-in-for-download' ); ?> <a href="http://www.doubleoptinfordownload.com/premium-double-opt-in-for-download/" target="new" ><?php _e( 'Click Here', 'double-opt-in-for-download' ); ?></a></h1>
+    </div>
+    <hr />
+</div>
