e8caa6be3b331c89011e28cabc510c94034ae937
andyba45 <andyba45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-05 17:02:52 +0000

    version 1.1.6 tags
    git-svn-id: https://plugins.svn.wordpress.org/double-opt-in-for-download/tags/1.1.6@943719 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ css/widget-style.php
@@ -0,0 +1,83 @@
+<?php if (version_compare(phpversion(), '5.4.19', '>')) : ?>
+<?php header( 'Content-type: text/css' ); ?>
+
+<?php require_once( '../../../../wp-blog-header.php' );
+$doifd_widget_option = get_option( 'doifd_lab_options' );
+?>
+<?php else : ?>
+
+<?php 
+require( '../../../../wp-blog-header.php' );
+
+header( 'Content-type: text/css' );
+
+$doifd_widget_option = get_option ( 'doifd_lab_options' ) ;
+?>
+<?php endif; ?>
+
+#widget_doifd_user_reg_form {
+    width: <?php if (isset($doifd_widget_option['widget_width'])) { echo $doifd_widget_option['widget_width']; }else{ echo '190'; }?>px;
+    margin-top: <?php if (isset($doifd_widget_option['widget_margin_top'])) { echo $doifd_widget_option['widget_margin_top']; }else{ echo '25'; }?>px;
+    margin-right: <?php if (isset($doifd_widget_option['widget_margin_right'])) { echo $doifd_widget_option['widget_margin_right']; }else{ echo '0'; }?>px;
+    margin-bottom: <?php if (isset($doifd_widget_option['widget_margin_bottom'])) { echo $doifd_widget_option['widget_margin_bottom']; }else{ echo '25'; }?>px;
+    margin-left: <?php if (isset($doifd_widget_option['widget_margin_left'])) { echo $doifd_widget_option['widget_margin_left']; }else{ echo '0'; }?>px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+     border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 15px rgba(68,68,68,0.6);
+    -moz-box-shadow: inset 0 1px 15px rgba(68,68,68,0.6);
+    box-shadow: inset 0 1px 15px rgba(68,68,68,0.6);
+    padding: <?php if (isset($doifd_widget_option['widget_inside_padding'])) { echo $doifd_widget_option['widget_inside_padding']; }else{ echo '5'; }?>px ;
+    background-color: <?php if (isset($doifd_widget_option['widget_background_color'])) { echo $doifd_widget_option['widget_background_color']; }else{ echo 'transparent'; }?>;
+}
+
+#widget_doifd_user_reg_form label {
+    color: <?php if (isset($doifd_widget_option['widget_color'])) { echo $doifd_widget_option['widget_color']; }else{ echo '#000000'; }?>;
+}
+
+#widget_doifd_user_reg_form ul li {
+    list-style-type: none;
+    padding: 0px;
+    margin: 0px;
+}
+
+#widget_doifd_user_reg_form input[type=text] {
+
+    width: <?php if (isset($doifd_widget_option['widget_input_width'])) { echo $doifd_widget_option['widget_input_width']; }else{ echo '180'; }?>px;
+    background: <?php if (isset($doifd_widget_option['widget_input_field_background_color'])) { echo $doifd_widget_option['widget_input_field_background_color']; }else{ echo 'transparent'; }?>;
+}    
+
+.doifd_widget_promo_link {
+    font-size: 0.7em !important;
+    padding-top: 15px !important;
+    text-align: center;
+}
+
+.doifd_widget_promo_link a:link,
+.doifd_widget_promo_link a:visited,
+.doifd_widget_promo_link a:hover,
+.doifd_widget_promo_link a:active {
+    color: #CCCCCC !important;
+}
+
+.widget_h4 {
+margin: 5px auto;
+text-align: center;
+width: 90%;
+color: <?php if ( isset( $doifd_widget_option[ 'widget_title_color' ] ) ) {
+    echo $doifd_widget_option[ 'widget_title_color' ];
+} else {
+    echo '#000000';
+} ?>;
+font-size: <?php if ( isset( $doifd_widget_option[ 'widget_title_size' ] ) ) {
+    echo $doifd_widget_option[ 'widget_title_size' ];
+} else {
+    echo '1em';
+} ?>;
+}
+
+.doifd_privacy_link {
+text-align: center;
+margin: 10px 0;
+font-size: <?php if (isset($doifd_widget_option['privacy_link_font_size'])) { echo $doifd_widget_option['privacy_link_font_size']; }else{ echo '0.9em'; }?>;
+}
---
+++ double-opt-in-for-download.php
@@ -0,0 +1,57 @@
+<?php
+/*
+  Plugin Name: Double Opt In For Download
+  Plugin URI: http://www.doubleoptinfordownload.com/
+  Description: Plugin for allowing a user to download a free promotional download in exchange for the users name and email address
+  Author: Lab Web Developement
+  Version: 1.1.6
+  Author URI: http://www.doubleoptinfordownload.com/
+  License: GPLv3
+
+  Double OPT-IN for download
+  Copyright (C) 2013-2014, Lab Web Development / Andy Bates - andy@labwebdesigns.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Define URL & DIR path to plugin folder and DIR path to download folder */
+
+$uploads = wp_upload_dir () ;
+
+define('ALLOW_INCLUDE', true);
+
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_URL' , plugin_dir_url ( __FILE__ ) ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_DIR' , plugin_dir_path ( __FILE__ ) ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_DOWNLOAD_DIR' , $uploads['basedir'] . '/doifd_downloads/' ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_DOWNLOAD_URL' , $uploads['baseurl'] . '/doifd_downloads/' ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_LANGUAGES_DIR' , dirname( plugin_basename( __FILE__ ) ) . '/languages/'  ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_CAPTCHA_URL' , plugin_dir_url ( __FILE__ ) . 'captcha/' ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_CAPTCHA_DIR' , plugin_dir_path ( __FILE__ ) . 'captcha/' ) ;
+define ( 'DOUBLE_OPT_IN_FOR_DOWNLOAD_IMG_URL' , plugin_dir_url ( __FILE__ ) . 'img/' ) ;
+
+include_once( DOUBLE_OPT_IN_FOR_DOWNLOAD_DIR . '/includes/class-doifd.php' );
+
+/* Register the activation hook for installing the plugin */
+
+register_activation_hook ( __FILE__, array( 'DoifdInstall', 'activate_doifd_plugin' ) );
+
+/* Add Additional links on the plugin admin screen */
+
+$filters = new DoifdFilters();
+
+add_filter('plugin_action_links_' . plugin_basename( __FILE__ ), array($filters, 'doifd_settings_link'));
+
+DOIFD::get_instance ();
+
+?>
